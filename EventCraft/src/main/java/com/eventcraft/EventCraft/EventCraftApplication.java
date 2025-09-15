366de0a3c57e19efb39fa3b0c02820081d2625f1
IT23656338 <it23656338@my.sliit.lk>
2025-09-15 15:12:09 +0530

    Initial Spring Boot setup

---
+++ EventCraft/src/main/java/com/eventcraft/EventCraft/EventCraftApplication.java
@@ -0,0 +1,13 @@
+package com.eventcraft.EventCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCraftApplication.class, args);
+	}
+
+}
